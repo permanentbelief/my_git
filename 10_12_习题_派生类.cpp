3ec95a6c1e213decfecdb21c2306a8ca477f2237
permenent <2691556679@qq.com>
2019-10-12 23:14:06 +0800

    Commit

---
+++ 10_12_习题_派生类.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+//#include <iostream>
+//using namespace std;
+//class Base
+//{
+//public:
+//	int fn1(){ cout << 1; return 0; }
+//protected:
+//	int fn2(){ cout << 2; return 0; }
+//private:
+//	int fn3(){ cout << 3; }
+//
+//};
+//class D1 : private Base
+//{
+//public:
+//	int fn1() { return Base::fn1(); }
+//	int fn2() { return Base::fn2(); }
+//	//int fn3() { return Base::fn3(); }
+//};
+//class D2 : protected Base
+//{
+//public:
+//	int fn1(){ return Base::fn1(); }
+//	int fn2(){ return Base::fn2(); }
+//	//int fn3(){ return Base::fn3(); }
+//
+//};
+//int main()
+//{
+//	Base a;
+//	a.fn1();
+//	//a.fn2();
+//	//a.fn3();
+//	cout << "= = == == = == = == =" << endl;
+//	D1 d1;
+//	d1.fn1();
+//	d1.fn2();
+//	//d1.fn3();
+//	cout << "=========================" << endl;
+//	D2 d2;
+//	d2.fn1();
+//	d2.fn2();
+//	//d2.fn3();
+//	system("pause");
+//
+//}
+ 
